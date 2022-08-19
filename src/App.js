1ba1677e84a37d9f41f5e827c85c3ae08309ea22
Andrei Florea <floreaalexandru146@gmail.com>
2022-08-19 18:51:33 +0200

    new changes

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hereo from './components/Hereo'
+import Analytics from './components/Analytics'
+import Newsletter from './components/Newsletter'
+import Cards from './components/Cards'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hereo />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
